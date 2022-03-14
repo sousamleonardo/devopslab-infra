c0c1b846ff9b440bf1b8445c3f8a11327437f2ad
sousamleonardo <65678348+sousamleonardo@users.noreply.github.com>
2022-03-14 15:24:14 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -20,3 +20,16 @@ resource "google_compute_instance" "firstvm" {
     }
   }
 }
+
+resource "google_app_engine_application" "app" {
+  project     = "labdevopscloud-6aso-grupo19"
+  location_id = "us-central"
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
+  location = "us-central1"
+  repository_id = "labdevops"
+  description = "Imagens Docker"
+  format = "DOCKER"
+}
--- providers.tf
+++ providers.tf
@@ -13,3 +13,9 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-b"
 }
+
+provider "google-beta" {
+  project = "labdevopscloud-6aso-grupo19"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
