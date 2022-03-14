65d75bb6f39b718640e96cba6cdf4510b9373cde
sousamleonardo <65678348+sousamleonardo@users.noreply.github.com>
2022-03-14 14:54:15 -0300

    Add config files for Terraform
    Terraform GCP Basics

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.13.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/gcpkey.json")
+
+  project = "labdevopscloud-6aso-grupo19"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
